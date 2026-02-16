1ae7c8e796f0216e9bfaf1737740d19f64f39f83
KennethVelazquez <kenny.arcelay@gmail.com>
2026-02-16 16:05:33 -0400

    add RecipeList, RecipeCard, database and server files

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,21 @@
-export default function RecipeCard() {
-    return (
-        <h1>Recipe Card</h1>
-    )
+import { Link } from 'react-router-dom'
+
+export default function RecipeCard({ recipe }) {
+  return (
+    <Link to={`/recipe/${recipe.id}`}>
+      <div className="recipe-card">
+      <img 
+        src={recipe.image && recipe.image !== 'null'
+          ? `http://localhost:3000/uploads/${recipe.image}`
+          : `./default-image.png`
+        }
+        alt={recipe.title}
+      />
+        <h2>{recipe.title}</h2>
+        <p>By {recipe.author}</p>
+        <p>Prep {recipe.prep_time}min| Cook {recipe.cook_time}min</p>
+        <hr />
+      </div>
+    </Link>
+  )
 }
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from 'react';
+import RecipeCard from "./RecipeCard"
+
 export default function RecipeList() {
-    return (
-        <h1>Recipe List</h1>
-    )
+
+const [recipes, setRecipes] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+
+async function fetchRecipes() {
+  try {
+    setLoading(true);
+    setError(null);
+
+    const response = await fetch("http://localhost:3000/api/recipes")
+    const data = await response.json()
+    setRecipes(data.recipes)
+    setLoading(false)
+
+  } catch (err) {
+    setError(`Error: ${err.message}`);
+    setLoading(false);
+  }
+}
+
+useEffect(() => {
+  // eslint-disable-next-line
+  fetchRecipes()
+}, [])
+
+if (loading) return <p>Loading recipes...</p>
+if (error) return <p>Error: {error}</p>
+
+  return (
+    <>
+      <h1>My Recipes</h1>
+      <div className="recipe-grid">
+        {recipes.map(recipe => (
+          <RecipeCard recipe={recipe} />
+        ))}
+      </div>
+    </>
+  )
 }
