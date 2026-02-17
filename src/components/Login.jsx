2ea3b1bd94e71d2caf020faeb8645942fe0d624b
KennethVelazquez <kenny.arcelay@gmail.com>
2026-02-17 00:11:21 -0400

    add Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,68 @@
-export default function Login() {
-    return (
-        <h1>Login</h1>
-    )
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Login({ setUser }) {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert('Login Successful!');
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        setUser(data.user);
+        navigate("/"); 
+      } else {
+        setError(data.error || 'Failed to login');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setError('Connection error. Make sure API is running!');
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {error && <p style={{color: "red"}}>{error}</p>}
+
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="loginEmail">Email</label>
+          <input 
+            type="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="loginPassword">Password</label>
+          <input 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  )
 }
