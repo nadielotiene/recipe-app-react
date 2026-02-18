f65d5c07653ad32d87cecbd793f756f2853c7ba5
KennethVelazquez <kenny.arcelay@gmail.com>
2026-02-18 18:45:31 -0400

    add search recipe functionality

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -4,6 +4,8 @@ import RecipeCard from "./RecipeCard"
 export default function RecipeList() {
 
 const [recipes, setRecipes] = useState([]);
+const [searchTerm, setSearchTerm] = useState("");
+const [selectedFilter, setSelectedFilter] = useState("all");
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
 
@@ -31,14 +33,39 @@ useEffect(() => {
 if (loading) return <p>Loading recipes...</p>
 if (error) return <p>Error: {error}</p>
 
+const filteredRecipes = recipes.filter(recipe => {
+  const matchesSearch =
+    recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    recipe.author.toLowerCase().includes(searchTerm.toLowerCase());
+
+  const matchesFilter = 
+    selectedFilter === "all" ||
+    recipe.difficulty === selectedFilter;
+
+  return matchesSearch && matchesFilter;
+});
+
   return (
     <>
-      <h1>My Recipes</h1>
       <div className="recipe-grid">
-        {recipes.map(recipe => (
-          <RecipeCard recipe={recipe} />
-        ))}
+        <h1>My Recipes</h1>
+        <input
+          type="text"
+          placeholder="Search recipes..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <br />
+        <br />
+        <button onClick={() => setSelectedFilter("all")}>All</button>
+        <button onClick={() => setSelectedFilter("easy")}>Easy</button>
+        <button onClick={() => setSelectedFilter("medium")}>Medium</button>
+        <button onClick={() => setSelectedFilter("hard")}>Hard</button>
       </div>
+        {filteredRecipes.map(recipe => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
     </>
   )
 }
